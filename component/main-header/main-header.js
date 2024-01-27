3518551c4c406d4c112931d595f56171155a11fe
unknown <ian52759@gmail.com>
2024-01-27 21:47:10 +0800

    2024/1/27

---
+++ component/main-header/main-header.js
@@ -0,0 +1,36 @@
+import Link from 'next/link';
+
+import logoImg from '@/assets/logo.png'
+import classes from './main-header.module.css'
+import Image from 'next/image';
+import MainHeaderBackground from './main-header-background';
+import NavLink from './nav-link';
+
+export default function MainHeader(){
+
+  return (
+    <>
+    <MainHeaderBackground/>
+    <header className={classes.header}>
+      <Link className = {classes.logo} href = "/">
+        <Image 
+          src = {logoImg} 
+          alt = "A plate with food on it" 
+          priority
+        />
+        Nextlevel food 
+      </Link>
+      <nav className={classes.nav}>
+        <ul>
+          <li>
+            <NavLink href = "/meals">Browse meals</NavLink>
+          </li>
+          <li>
+            <NavLink href = "/community">Foodies community</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    </>
+  )
+}
